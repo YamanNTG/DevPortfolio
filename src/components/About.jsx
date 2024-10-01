20d31f627f1c16ca613a7db2b5de468734ba8304
YamanNTG <claudiuoprea21@gmail.com>
2024-10-02 00:13:19 +0100

    About and Contact updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,55 @@ const About = () => {
       <article>
         <SectionTitle text="about me" />
         <p className="text-slate-600 mt-8 leading-loose">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint maiores
-          itaque doloribus asperiores dolorum quas voluptates distinctio, porro
-          beatae quam provident libero animi totam, praesentium voluptatem
-          corrupti dignissimos ipsum accusantium!
+          Hi, my name is{" "}
+          <span className="font-bold text-slate-800">Claudiu</span>, and I’m a
+          <span className="font-bold text-slate-800">
+            {" "}
+            27-year-old self-taught developer{" "}
+          </span>
+          with a passion for{" "}
+          <span className="text-teal-700 font-semibold">
+            {" "}
+            building web applications{" "}
+          </span>
+          . Over the past year and a half, I’ve{" "}
+          <span className="font-bold"> immersed myself in coding </span>,
+          learning everything from the basics of HTML and CSS to more advanced
+          frameworks and tools such as
+          <span className="text-teal-700 font-semibold">
+            {" "}
+            JavaScript, TypeScript, jQuery, React, Tailwind, Next.js, Prisma,
+            MongoDB, Git, and GitHub{" "}
+          </span>
+          . My journey started with{" "}
+          <span className="italic"> countless YouTube videos </span>, learning
+          platforms, and a series of in-depth courses on Udemy. I’ve developed a
+          strong proficiency in front-end technologies and enjoy creating
+          responsive, user-friendly interfaces. Now, I’m shifting my focus to
+          <span className="text-teal-700 font-semibold">
+            {" "}
+            mastering the back-end with Node.js, Express, and Nest.js{" "}
+          </span>
+          to become a well-rounded full-stack developer. I’m committed to{" "}
+          <span className="font-bold"> honing my skills </span>
+          and delivering{" "}
+          <span className="text-teal-700 font-semibold">
+            {" "}
+            clean, efficient code{" "}
+          </span>{" "}
+          that powers intuitive applications. In the coming months, I’ll be
+          continuing my studies and expanding my skill set with the goal of
+          becoming a{" "}
+          <span className="font-bold text-slate-800">
+            {" "}
+            full-stack developer{" "}
+          </span>{" "}
+          who can confidently handle both{" "}
+          <span className="font-bold text-slate-800">
+            {" "}
+            front-end and back-end tasks{" "}
+          </span>
+          .
         </p>
       </article>
     </section>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,13 +8,33 @@ const Contact = () => {
         <img src={contact} className="h-80 lg:h-96" />
       </div>
       <article>
-        <SectionTitle text="Contact" />
+        <SectionTitle text="Get in touch" />
         <p className="text-slate-600 mt-8 leading-loose">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint maiores
-          itaque doloribus asperiores dolorum quas voluptates distinctio, porro
-          beatae quam provident libero animi totam, praesentium voluptatem
-          corrupti dignissimos ipsum accusantium!
+          Whether you're looking to{" "}
+          <span className="font-bold text-slate-800">
+            {" "}
+            collaborate on a project{" "}
+          </span>
+          , have a question, or just want to chat, feel free to reach out! I’m
+          always open to
+          <span className="text-teal-700 font-semibold">
+            {" "}
+            discussing new opportunities and ideas{" "}
+          </span>
+          .
         </p>
+        <ul className="mt-4">
+          <li className="font-bold text-slate-800">Email: </li>
+          <li className="font-bold text-slate-800">LinkedIn:</li>
+        </ul>
+        <div className="mt-4 flex flex-col items-center capitalize">
+          <button className="btn btn-wide bg-teal-700 text-white hover:bg-teal-500 hover:text-yellow-100">
+            Download CV
+          </button>
+          <p className="text-slate-600 leading-loose font-bold">
+            Looking forward to hearing from you!
+          </p>
+        </div>
       </article>
     </section>
   );
